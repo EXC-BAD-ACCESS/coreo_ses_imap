21031424382d5893492b8f79fe53883a5a9724e3
Adam Taylor <adam@appsorcery.com>
2017-07-12 19:29:32 -0700

    fixes path issue

--- boot-scripts/setup_forever.sh
+++ boot-scripts/setup_forever.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+coreo_dir="$(pwd)"
+cd /var/app/current
 
-echo "forever-service install $APP_NAME --script /var/app/current/start.js -o \" $APP_STARTUP_ARGS\""
-forever-service install $APP_NAME --script /var/app/current/start.js -o " $APP_STARTUP_ARGS"
+echo "forever-service install $APP_NAME --script start.js -o \" $APP_STARTUP_ARGS\""
+forever-service install $APP_NAME --script start.js -o " $APP_STARTUP_ARGS"
 service $APP_NAME start
 
-echo "forever-service install renderer --script /var/app/current/renderer.js"
-forever-service install renderer --script /var/app/current/renderer.js
+echo "forever-service install renderer --script renderer.js"
+forever-service install renderer --script renderer.js
 service renderer start
+
+cd $coreo_dir
