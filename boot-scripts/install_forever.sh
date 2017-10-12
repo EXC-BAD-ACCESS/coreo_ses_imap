6fd40ff3a7eec0945f2c8b6221663a68c1e3b2bb
Adam Taylor <ataylor@ip-192-168-0-12.ec2.internal>
2017-10-12 16:00:25 -0700

    changes

--- boot-scripts/install_forever.sh
+++ boot-scripts/install_forever.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#npm install sails forever forever-service -g
 npm install forever forever-service -g
-#ln -s /usr/local/bin/sails /usr/bin/sails
+
 ln -s /usr/local/bin/forever /usr/bin/forever
 ln -s /usr/local/bin/forever-service /usr/bin/forever-service
--- boot-scripts/setup_redis.sh
+++
@@ -1,4 +0,0 @@
-if [ "$INSTALL_REDIS" == "true" ]; then
-  echo "redis-server --daemonize yes"
-  redis-server --daemonize yes
-fi
